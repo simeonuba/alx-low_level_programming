c508acc345246f5b7d85c72c6c7413ef40b2a3f8
simeonuba <101792434+simeonuba@users.noreply.github.com>
2022-04-10 05:41:13 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * *main - Entry point
- * *
- * *Return: Always 0 (Success)
- * */
-
+ * main - Entry poin
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
