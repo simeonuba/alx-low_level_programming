7ebd0d2de892f8f284e3bc5ed2248e2ba67b22dc
simeon uba <simeonphilipuba@gmail.com>
2022-04-10 12:03:58 -0700

    if else

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Aways 0 (Sucess)
+ *     */
+int main(void)
+{
+		char ch = 'a';
+
+			while (ch <= 'z')
+					{
+								if (ch != 'e' && ch != 'q')
+											{
+															putchar(ch);
+																	}
+										ch++;
+											}
+				putchar('\n');
+					return (0);
+}
+
