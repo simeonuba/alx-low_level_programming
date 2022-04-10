590998e585c2e8ac16013a34e76e0e85ba087d56
simeonuba <101792434+simeonuba@users.noreply.github.com>
2022-04-10 05:40:34 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * * main - Entry point
- * *
- * * Return: Always 0 (Success)
- * */
+ * main - Entry poin
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
